d79c76b3b1784ab28101d59271d802dc2e49ddb2
Timofey Mukha <timofey.mukha@protonmail.com>
2024-01-25 10:23:51 +0300

    Add dns download script

---
+++ dns/download.sh
@@ -0,0 +1,5 @@
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_mean_prof.dat
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_vel_fluc_prof.dat
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_vor_pres_fluc_prof.dat
+wget http://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_1d_energy_spectra.h5
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_RSTE_k_prof.dat
