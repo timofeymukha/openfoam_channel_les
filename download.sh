5854bdc793505d5d85b0c4bb932a09b17fe426e7
Timofey Mukha <timofey.mukha@protonmail.com>
2024-10-31 17:06:52 +0300

    Initial add of new data

---
+++ download.sh
@@ -0,0 +1,17 @@
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_mean_prof.dat
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_vel_fluc_prof.dat
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_vor_pres_fluc_prof.dat
+wget http://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_1d_energy_spectra.h5
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_1000_RSTE_k_prof.dat
+
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0550_mean_prof.dat
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0550_vel_fluc_prof.dat
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0550_vor_pres_fluc_prof.dat
+wget http://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0550_1d_energy_spectra.h5
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0550_RSTE_k_prof.dat
+
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0180_mean_prof.dat
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0180_vel_fluc_prof.dat
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0180_vor_pres_fluc_prof.dat
+wget http://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0180_1d_energy_spectra.h5
+wget https://turbulence.oden.utexas.edu/channel2015/data/LM_Channel_0180_RSTE_k_prof.dat
